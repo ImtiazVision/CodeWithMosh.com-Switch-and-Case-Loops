574f90e4fd927cfcab5ebfc3e7d03b691b4a3af5
Imtiaz A <ahmedimtiaz.ahmed@gmail.com>
2020-12-05 02:25:55 +0000

    fixed the indentations

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-// let role = "moderator";
+let role = "moderator";
 
-// switch (role) {
-// 	case 'guest':
-// 		console.log('Guest User');
-// 		break;
-// 	case 'moderator':
-// 		console.log('Moderator User');
-// 		break;
-// 	default:
-// 		console.log("Unknown User");
+switch (role) {
+	case 'guest':
+		console.log('Guest User');
+		break;
+	case 'moderator':
+		console.log('Moderator User');
+		break;
+	default:
+		console.log("Unknown User");
 
-// }
+}
 
-// if (role === 'guest') console.log('Guest User');
-// else if (role === 'moderator') console.log("Moderator User");
-// else console.log("Unknown User");
+if (role === 'guest') console.log('Guest User');
+else if (role === 'moderator') console.log("Moderator User");
+else console.log("Unknown User");
 
 
 /* For Loops */
 
 // let i = 0 is the initialization | i < 5 is the condition | i++ is incrementation by 1
-//for (let i = 0; i <= 5; i++) {
-// console.log("Hello World", i);
-// if (i % 2 == 0) console.log(i); // output all even numbers
-// if (i % 2 !== 0) console.log(i); // output all odd numbers
-//}
+for (let i = 0; i <= 5; i++) {
+	console.log("Hello World", i);
+	if (i % 2 == 0) console.log(i); // output all even numbers
+	if (i % 2 !== 0) console.log(i); // output all odd numbers
+}
 
-// for (let i = 6; i >= 1; i--){       // i-- is decrementation by 1
-// if (i % 2 == 0) console.log(i);
-// }
+for (let i = 6; i >= 1; i--) {       // i-- is decrementation by 1
+	if (i % 2 == 0) console.log(i);
+}
 
 /* While Loops */
 
-// let i = 0;  // we initialize i variable externally and outside of the loop function. i has a global scope which means it is accessible globally/outside of for loop and inside of for loop as well//
+let i = 0;  // we initialize i variable externally and outside of the loop function. i has a global scope which means it is accessible globally/outside of for loop and inside of for loop as well//
 
-// while (i <= 5) {  // this is the condition//
-// if (i % 2 !== 0) console.log(i);
-// i++; // this is the incrementation by 1
-// }
+while (i <= 5) {  // this is the condition//
+	if (i % 2 !== 0) console.log(i);
+	i++; // this is the incrementation by 1
+}
 
 /* Do-While Loops */
 
